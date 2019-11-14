165062421d38f7325ad5e566a7f322658e0d2b51
Benjamin1361 <57761944+Benjamin1361@users.noreply.github.com>
2019-11-14 14:51:18 +0000

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,91 @@
+"""
+
+print('hello')
+print('all around the world'[8].upper())
+
+name = 'Ben'
+
+print(name)
+age=27
+print(age)
+fav_food='kebab'
+print(fav_food)
+print('my_fav_food is {}'.format(fav_food))
+print('hello my name is {} and i ma {} years old. {} is my fav food'.format(name, age, fav_food))
+i=10
+i=i+2
+i +=2
+print(i)
+subtract= 10
+subtract -= 5
+print(subtract)
+fav_drink= 'wine'
+print(fav_drink)
+my_fav_sport= 'football'
+print('may name is {} and {} is my fav sport'.format(name,my_fav_sport))
+name= 'Korosh'
+age= '37'
+favourit_colour= 'red'
+my_friend= 'korosh'
+print('my friend is {},he is {} and he likes {} as favourit colour'.format (my_friend,age,favourit_colour))
+breackfast='milk and toast'
+lunch='chiken and rice'
+dinner='just sald'
+print('I had {} for breackfast, {} for luch and {} for dinner as i can not have heavy meal by night time'.format(breackfast,lunch,dinner))
+print(" ")
+breackfast='egg'
+lunch='sandwich'
+dinner='some fruit'
+print('I hava plan for tomorrow to have {} for breackfast,{} for lunch time and just {} for dinner'.format(breackfast,lunch,dinner))
+from datetime import date
+d1=date(1982,12,29)
+d2=date(2019,11,12)
+delta = d2 - d1
+print(delta.days)
+
+
+
+
+print ('        |        |        ')
+print ('        |        |        ')
+print ('        |        |        ')
+print ('--------------------------')
+print ('        |        |        ')
+print ('        |        |        ')
+print ('        |        |        ')
+print ('--------------------------')
+print ('        |        |        ')
+print ('        |        |        ')
+print ('        |        |        ')
+
+"""
+space1 = 'o'
+space2 = 'x'
+space3 = ''
+space4 = ''
+space5 = 'o'
+space6 = 'x'
+space7 = 'x'
+space8 = 'o'
+space9 = ''
+print ('------------------------')
+print ('        |        |      ')
+print ('{}       |    {}  |   {}   '.format(space1, space2, space3))
+print ('        |        |      ')
+print ('        |        |      ')
+print ('------------------------')
+print ('        |        |      ')
+print ('        |        |      ')
+print ('  {}      | {}      {} |'.format(space4,space5,space6))
+print ('        |        |      ')
+print ('------------------------')
+print ('        |        |      ')
+print ('        |        |      ')
+print ('        |        |      ')
+print ('        |        |      ')
+print ('------------------------')
+
+
+
+
+
